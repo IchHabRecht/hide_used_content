2d9556c663d2b285f7260ea4887572069e5fa2f3
Nicole Cordes <typo3@cordes.co>
2018-10-30 10:58:36 +0100

    [RELEASE] Release of hide_used_content 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "hide_used_content".
  *
- * Auto generated 30-10-2018 03:12
+ * Auto generated 30-10-2018 10:58
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -35,6 +35,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:7:{s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"3356";s:13:"composer.json";s:4:"ac6a";s:12:"ext_icon.png";s:4:"ad17";s:16:"phpunit.xml.dist";s:4:"7d0d";s:24:"sonar-project.properties";s:4:"bd01";s:36:"Resources/Public/Icons/Extension.svg";s:4:"0e56";}',
+  '_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"d15a";s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"3356";s:13:"composer.json";s:4:"91c4";s:12:"ext_icon.png";s:4:"ad17";s:17:"ext_localconf.php";s:4:"7562";s:16:"phpunit.xml.dist";s:4:"7d0d";s:24:"sonar-project.properties";s:4:"bd01";s:30:"Classes/Cache/CacheManager.php";s:4:"b7f6";s:36:"Classes/Hooks/PageLayoutViewHook.php";s:4:"d109";s:30:"Classes/Slot/TcaColPosSlot.php";s:4:"bd2a";s:36:"Resources/Public/Icons/Extension.svg";s:4:"0e56";}',
 );
 
