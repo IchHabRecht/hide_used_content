c826f49f30d87caa43326b45efc8b93afd7094fe
Nicole Cordes <typo3@cordes.co>
2018-10-30 03:12:46 +0100

    [TASK] Add basic extension files
    Add supporting files from EXT:my_skeleton 0.1.6

---
+++ README.md
@@ -0,0 +1,8 @@
+# TYPO3 Extension hide_used_content
+
+[![Latest Stable Version](https://img.shields.io/packagist/v/ichhabrecht/hide-used-content.svg)](https://packagist.org/packages/ichhabrecht/hide-used-content)
+[![Build Status](https://img.shields.io/travis/IchHabRecht/hide_used_content/master.svg)](https://travis-ci.org/IchHabRecht/hide_used_content)
+[![StyleCI](https://styleci.io/repos/155286928/shield?branch=master)](https://styleci.io/repos/155286928)
+
+The extension removes content elements that have an own colPos configuration (e.g. nested content elements) from the
+"Unused" column and hides wrong "Unused elements detected on this page" notification. 
---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "hide_used_content".
+ *
+ * Auto generated 30-10-2018 03:12
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Hide used content elements',
+  'description' => 'Removes used content elements with own colPos configuration from "Unused" column',
+  'category' => 'misc',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'biz-design',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '0.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '9.5.0-9.5.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:7:{s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"3356";s:13:"composer.json";s:4:"ac6a";s:12:"ext_icon.png";s:4:"ad17";s:16:"phpunit.xml.dist";s:4:"7d0d";s:24:"sonar-project.properties";s:4:"bd01";s:36:"Resources/Public/Icons/Extension.svg";s:4:"0e56";}',
+);
+
