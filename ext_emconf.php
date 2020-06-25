42a84c0f92853029992e83e275f772d73366e38e
Nicole Cordes <typo3@cordes.co>
2020-06-25 15:18:55 +0200

    [RELEASE] Release of hide_used_content 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "hide_used_content".
  *
- * Auto generated 30-10-2018 20:08
+ * Auto generated 25-06-2020 15:18
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,20 +21,20 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '0.2.0',
-  'constraints' => 
+  'version' => '1.0.0',
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
       'typo3' => '9.5.0-10.4.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"9472";s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"3356";s:13:"composer.json";s:4:"91c4";s:12:"ext_icon.png";s:4:"ad17";s:17:"ext_localconf.php";s:4:"7562";s:16:"phpunit.xml.dist";s:4:"7d0d";s:24:"sonar-project.properties";s:4:"bd01";s:30:"Classes/Cache/CacheManager.php";s:4:"b7f6";s:36:"Classes/Hooks/PageLayoutViewHook.php";s:4:"8a3e";s:30:"Classes/Slot/TcaColPosSlot.php";s:4:"bd2a";s:36:"Resources/Public/Icons/Extension.svg";s:4:"0e56";}',
+  '_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"8282";s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"3356";s:13:"composer.json";s:4:"817d";s:12:"ext_icon.png";s:4:"ad17";s:17:"ext_localconf.php";s:4:"7562";s:16:"phpunit.xml.dist";s:4:"041c";s:24:"sonar-project.properties";s:4:"bd01";s:30:"Classes/Cache/CacheManager.php";s:4:"b7f6";s:36:"Classes/Hooks/PageLayoutViewHook.php";s:4:"8a3e";s:30:"Classes/Slot/TcaColPosSlot.php";s:4:"bd2a";s:36:"Resources/Public/Icons/Extension.svg";s:4:"0e56";}',
 );
 
